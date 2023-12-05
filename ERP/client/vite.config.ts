cfe87b151ec4cd95e05ac33cd77732842c961de4
Staticaldust <139761510+Staticaldust@users.noreply.github.com>
2023-12-05 11:25:16 +0200

    env update

--- ERP/client/vite.config.ts
+++ ERP/client/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
-    base: "/erp",
+    base: "erp",
     server: {
       watch: {
         usePolling: true,
